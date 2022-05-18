f2a7338c302e9bd4cbeaf6b46752626bbb1e448c
arvind <walkalone.arvind@gmail.com>
2022-05-18 21:40:03 +0530

    switch case

--- CsharpTraining/Program.cs
+++ CsharpTraining/Program.cs
@@ -18,8 +18,8 @@ class Program
         */
 
         // get min and max
-        //Console.WriteLine(int.MinValue);
-        //Console.WriteLine(int.MaxValue);
+        ////Console.WriteLine(int.MinValue);
+        ////Console.WriteLine(int.MaxValue);
 
         // Implicit Conversion
         byte num = 100;
@@ -32,9 +32,9 @@ class Program
         int num4 = 0;
         bool result = int.TryParse(num2, out num4);
 
-        //Console.WriteLine(num1);
-        //Console.WriteLine(num3);
-        //Console.WriteLine(num4);
+        ////Console.WriteLine(num1);
+        ////Console.WriteLine(num3);
+        ////Console.WriteLine(num4);
 
         #endregion
 
@@ -54,15 +54,15 @@ class Program
 
         if (num5 < 7)
         {
-            //Console.WriteLine("if block");
+            ////Console.WriteLine("if block");
         }
         else
         {
-            //Console.WriteLine("else block");
+            ////Console.WriteLine("else block");
         }
 
         int r = num5 > 7 ? 100 : 200;
-        Console.WriteLine(r);
+        //Console.WriteLine(r);
 
         #endregion
 
@@ -72,22 +72,61 @@ class Program
 
         if (age == 9)
         {
-            Console.WriteLine("your age is 9");
+            //Console.WriteLine("your age is 9");
         }
         else if (age == 10)
         {
-            Console.WriteLine("your age is 10");
+            //Console.WriteLine("your age is 10");
         }
         else if (age == 15)
         {
-            Console.WriteLine("your age is 15");
+            //Console.WriteLine("your age is 15");
         }
         else if (age == 20)
         {
-            Console.WriteLine("your age is 20");
+            //Console.WriteLine("your age is 20");
         }
 
 
+
+        #endregion
+
+        #region switch case
+
+        /* 
+        int salary = 110;
+
+        if(300 == salary || 200 == salary)
+        {
+            Console.WriteLine("you dont have bonus becouse your salary is " + salary);
+        }
+        else if(100 == salary)
+        {
+            Console.WriteLine("congratulation : you have bonus becouse your salary is 100");
+        }
+        else {
+            Console.WriteLine("your salary is not found");
+        }
+        */
+
+        /* */
+       
+        Console.WriteLine("Please enter your status");
+        string accountStatus = Console.ReadLine().ToLower();
+        switch (accountStatus)
+        {
+            case "active":
+                Console.WriteLine("Login Successfully");
+                break;
+            case "pending":
+            case "cancel":
+            case "reject":
+                Console.WriteLine("your dont login because your account status is " + accountStatus);
+                break;
+            default:
+                Console.WriteLine("your account status not found, please contact admin");
+                break;
+        }
 
         #endregion
 
