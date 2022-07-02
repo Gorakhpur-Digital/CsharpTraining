2f3316662b94903619b624a9270789f4cd1623ca
arvind <walkalone.arvind@gmail.com>
2022-07-02 12:35:28 +0530

    late binding example

--- CsharpTraining/Program.cs
+++ CsharpTraining/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using CsharpTraining.Bl;
 using CsharpTraining.Model;
 
@@ -7,43 +8,22 @@ class Program
 {
     public static void Main()
     {
-        List<Employee> empList = new List<Employee>();
-
-        empList.Add(new Employee()
-        {
-            EmployeeId = 1,
-            Name = "Arvind",
-            MobileNo = "1234567"
-        });
-
-        empList.Add(new Employee()
-        {
-            EmployeeId = 2,
-            Name = "Abhay",
-            MobileNo = "9876543"
-        });
-
-        empList.Add(new Employee()
-        {
-            EmployeeId = 3,
-            Name = "Vishal",
-            MobileNo = "37654329"
-        });
-
-        //Employee e =  empList[1];
-        //Console.WriteLine(e.Name);
-
-        Console.WriteLine("{0} {1} {2}", empList[0].EmployeeId, empList[0].Name, empList[0].MobileNo);
-
-        
-
+        // late binding example
+        Assembly excutingAssemply = Assembly.GetExecutingAssembly();
+        Type customerType = excutingAssemply.GetType("Customer");
+        object customerInstance = Activator.CreateInstance(customerType);
+        MethodInfo getFullNameMethod = customerType.GetMethod("GetFullName");
+        string[] parameter = { "Gorakhpur", "Digital" };
+        string fullName = (string)getFullNameMethod.Invoke(customerInstance,parameter);
+        Console.WriteLine(fullName);
     }
-
 }
 
-class Employee
+public class Customer
 {
-    public int EmployeeId { get; set; }
-    public string Name { get; set; }
-    public string MobileNo { get; set; }
+    public string GetFullName(string firstName, string lastName)
+    {
+        return firstName + " " + lastName;
+    }
+
 }
