5979222115e635fc75bea98834b81edc3e13d611
arvind <walkalone.arvind@gmail.com>
2022-07-04 12:05:55 +0530

    Indexers examples

---
+++ CsharpTraining/Bl/EmployeeBl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CsharpTraining.Bl
+{
+    class EmployeeBl
+    {
+        List<Model.EmployeeModel> employees = new List<Model.EmployeeModel>() {
+                new Model.EmployeeModel {EmployeeID = 1, FirstName = "Arvind" },
+                new Model.EmployeeModel {EmployeeID = 2, FirstName = "Vishal" },
+                new Model.EmployeeModel {EmployeeID = 3, FirstName = "Abhay" },
+                new Model.EmployeeModel {EmployeeID = 4, FirstName = "Ankit" },
+                new Model.EmployeeModel {EmployeeID = 5, FirstName = "Mohit" },
+                new Model.EmployeeModel {EmployeeID = 6, FirstName = "Aman" },
+            };
+
+        public string this[int employeeId]
+        {
+            get
+            {
+                return employees.FirstOrDefault(x => x.EmployeeID.Equals(employeeId)).FirstName;
+            }
+            set
+            {
+                employees.FirstOrDefault(x => x.EmployeeID.Equals(employeeId)).FirstName = value;
+            }
+        }
+        public string this[string employeeName]
+        {
+            get
+            {
+                return employees.FirstOrDefault(x => x.FirstName.Equals(employeeName)).FirstName;
+            }
+            set
+            {
+                employees.FirstOrDefault(x => x.FirstName.Equals(employeeName)).FirstName = value;
+            }
+        }
+
+    }
+}
--- CsharpTraining/Program.cs
+++ CsharpTraining/Program.cs
@@ -8,12 +8,9 @@ class Program
 {
     public static void Main()
     {
-
-        Employee employee = new Employee();
-        employee.FirstName = "Gorakhpur";
-        employee.LastName = "Digital";
-
-        string fullName = employee.GetFullEmployeeName();
-        Console.WriteLine(fullName);
+        //indexers examples
+        EmployeeBl employeeBl = new EmployeeBl();
+        string name = employeeBl[1];
+        Console.WriteLine(name);        
     }
 }
