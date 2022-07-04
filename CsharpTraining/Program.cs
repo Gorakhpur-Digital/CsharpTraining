0f5d7dc2e32a68c222bb7e15204298e48d0ded34
arvind <walkalone.arvind@gmail.com>
2022-07-04 12:57:09 +0530

    optional parameter example

--- CsharpTraining/Program.cs
+++ CsharpTraining/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using CsharpTraining.Bl;
 using CsharpTraining.Model;
 
@@ -8,9 +9,52 @@ class Program
 {
     public static void Main()
     {
-        //indexers examples
-        EmployeeBl employeeBl = new EmployeeBl();
-        string name = employeeBl[1];
-        Console.WriteLine(name);        
+
+        AddNumbers1(10, 10, 10, 10);
+    }
+
+    // example one for optional parameter
+    public static void AddNumbers1(int num1, int num2, params int[] otherNumbers)
+    {
+        int result = num1 + num2;
+        if(otherNumbers != null)
+        {
+            foreach(int num in otherNumbers)
+            {
+                result += num;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+
+    // example two for optional parameter
+    public static void AddNumbers2(int num1, int num2, int[] otherNumbers = null)
+    {
+        int result = num1 + num2;
+        if (otherNumbers != null)
+        {
+            foreach (int num in otherNumbers)
+            {
+                result += num;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+
+    // example three for optional parameter
+    public static void AddNumbers3(int num1, int num2, [Optional] int [] otherNumbers)
+    {
+        int result = num1 + num2;
+        if (otherNumbers != null)
+        {
+            foreach (int num in otherNumbers)
+            {
+                result += num;
+            }
+        }
+
+        Console.WriteLine(result);
     }
 }
